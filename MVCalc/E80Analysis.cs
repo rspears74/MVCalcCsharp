63a2585cd8c70d4c922ceff2b22fc0f4389f940e
rspears74 <rspears690@gmail.com>
2019-03-22 19:55:25 -0600

    refactored code base - added Train and E80Analysis classes

---
+++ MVCalc/E80Analysis.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVCalc
+{
+    class E80Analysis
+    {
+
+        public Train AnalysisTrain { get; set; }
+        public double Span { get; set; }
+        public double IncrementInches { get; set; }
+        public double ImpactFactor { get; set; }
+        public double DistFactor { get; set; }
+        public TrainConfig TrainType { get; set; }
+
+        public E80Analysis() { }
+
+        public void GetTrain()
+        {
+            AnalysisTrain = Train.GetAltE80Train();
+
+            if (TrainType == TrainConfig.E80)
+            {
+                AnalysisTrain = Train.GetE80Train();
+                AnalysisTrain.AddE80TrailingLoad(Span);
+            }
+        }
+
+        public Dictionary<string, double> CalculateSingleLocation(double xLocation)
+        {
+            AnalysisTrain.SetAxlePositions();
+            double incr = IncrementInches / 12;
+            double mMaxVal = 0;
+            double vMaxVal = 0;
+            double mMaxLoc = 0;
+            double vMaxLoc = 0;
+
+            while (AnalysisTrain.AxlePositions.Min() < Span)
+            {
+                List<double> loadsOnBridge = new List<double>();
+                List<double> axlesOnBridge = new List<double>();
+                for (int j = 0; j < AnalysisTrain.AxlePositions.Count(); j++)
+                {
+                    if (AnalysisTrain.AxlePositions[j] > 0 && AnalysisTrain.AxlePositions[j] < Span)
+                    {
+                        loadsOnBridge.Add(AnalysisTrain.AxleLoads[j]);
+                        axlesOnBridge.Add(AnalysisTrain.AxlePositions[j]);
+                    }
+                }
+
+                double[] moments = new double[loadsOnBridge.Count()];
+                double[] shears = new double[loadsOnBridge.Count()];
+                for (int j = 0; j < loadsOnBridge.Count(); j++)
+                {
+                    moments[j] = Moment(loadsOnBridge[j], xLocation, axlesOnBridge[j], Span);
+                    shears[j] = Shear(loadsOnBridge[j], xLocation, axlesOnBridge[j], Span);
+                }
+
+                double momentTotal = moments.Sum();
+                double shearTotal = Math.Abs(shears.Sum());
+
+                if (momentTotal > mMaxVal)
+                {
+                    mMaxVal = momentTotal;
+                    mMaxLoc = AnalysisTrain.AxlePositions[0];
+                }
+                if (shearTotal > vMaxVal)
+                {
+                    vMaxVal = shearTotal;
+                    vMaxLoc = AnalysisTrain.AxlePositions[0];
+                }
+                AnalysisTrain.Increment(incr);
+            }
+
+            double mMax = mMaxVal * ImpactFactor * DistFactor;
+            double vMax = vMaxVal * ImpactFactor * DistFactor;
+            Dictionary<string, double> result = new Dictionary<string, double>();
+            result.Add("m", mMax);
+            result.Add("v", vMax);
+            result.Add("mloc", mMaxLoc);
+            result.Add("vloc", vMaxLoc);
+            return result;
+        }
+
+        public Tuple<double[], double[]> CalculateEnvelope(int plotPoints)
+        {
+            double[] locations = new double[plotPoints + 1];
+            double[] mMaxs = new double[plotPoints + 1];
+            double[] vMaxs = new double[plotPoints + 1];
+
+            for (int i = 0; i < plotPoints + 1; ++i)
+            {
+                locations[i] = Span / (plotPoints) * i;
+                Dictionary<string, double> vals = CalculateSingleLocation(locations[i]);
+                mMaxs[i] = vals["m"];
+                vMaxs[i] = vals["v"];
+            }
+
+            return new Tuple<double[], double[]>(mMaxs, vMaxs);
+        }
+
+        public void GetTrainType(int type)
+        {
+            if (type == 0)
+            {
+                TrainType = TrainConfig.E80;
+            }
+            else
+            {
+                TrainType = TrainConfig.E80Alt;
+            }
+        }
+
+        public static double Moment(double P, double loc, double x, double L)
+        {
+            double R = P * x / L;
+            if (loc < x)
+            {
+                return R * (L - loc) - P * (x - loc);
+            }
+            else
+            {
+                return R * (L - loc);
+            }
+        }
+
+        public static double Shear(double P, double loc, double x, double L)
+        {
+            double R = P * x / L;
+            if (loc < x)
+            {
+                return P - R;
+            }
+            else
+            {
+                return -R;
+            }
+        }
+
+    }
+}
--- MVCalc/MainWindow.xaml.cs
+++ MVCalc/MainWindow.xaml.cs
@@ -66,151 +66,49 @@ namespace MVCalc
             LineSeries series = new LineSeries();
             int numPoints = vals.Length;
 
-            for (int i=0; i < numPoints; ++i)
+            for (int i = 0; i < numPoints; ++i)
             {
-                series.Points.Add(new DataPoint((spanLength / (numPoints-1) * i), vals[i]));
+                series.Points.Add(new DataPoint((spanLength / (numPoints - 1) * i), vals[i]));
             }
             model.Series.Add(series);
             return model;
         }
 
-        public static double Moment(double P, double loc, double x, double L)
-        {
-            double R = P * x / L;
-            if (loc < x)
-            {
-                return R * (L - loc) - P * (x - loc);
-            }
-            else
-            {
-                return R * (L - loc);
-            }
-        }
+        public static PlotModel MPlot { get; set; }
 
-        public static double Shear(double P, double loc, double x, double L)
-        {
-            double R = P * x / L;
-            if (loc < x)
-            {
-                return P - R;
-            }
-            else
-            {
-                return -R;
-            }
-        }
+        public static PlotModel VPlot { get; set; }
 
-        public Dictionary<string, double> CalculateLocation(double xLocation)
+        public void CalculateClick(object sender, RoutedEventArgs e)
         {
-            double spanLength = 0;
-            double incr = 0;
-            double impactFactor = 0;
-            double distFactor = 0;
+            // declare/instantiate variables
+            E80Analysis analysis = new E80Analysis();
+            double xLocation;
+            bool? pct;
 
+            // get user inputs
             try
             {
-                spanLength = double.Parse(SpanLength.Text);
-                incr = double.Parse(Increment.Text) / 12;
-                impactFactor = double.Parse(ImpactFactor.Text);
-                distFactor = double.Parse(DistributionFactor.Text);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Invalid input!");
-            }
-
-            List<int> axleLoads =     new List<int> { 40, 80, 80, 80, 80, 52, 52, 52, 52,
-                                                      40, 80, 80, 80, 80, 52, 52, 52, 52, 8 };
-            List<double> axleSpaces = new List<double> { 0, 8, 5, 5, 5, 9, 5, 6, 5,
-                                                         8, 8, 5, 5, 5, 9, 5, 6, 5, 5.5 };
-
-            int numAxles = axleSpaces.Count();
-
-            double[] axlePositions = new double[numAxles];
-            axlePositions[0] = -axleSpaces[0];
-            for (int i = 1; i < numAxles; i++)
-            {
-                    axlePositions[i] = axlePositions[i - 1] - axleSpaces[i];
-            }
-            double mMaxVal = 0;
-            double vMaxVal = 0;
-            double mMaxLoc = 0;
-            double vMaxLoc = 0;
-
-            while (axlePositions.Min() < spanLength)
-            {
-                List<double> loadsOnBridge = new List<double>();
-                List<double> axlesOnBridge = new List<double>();
-                for (int j = 0; j < axlePositions.Count(); j++)
-                {
-                    if (axlePositions[j] > 0 && axlePositions[j] < spanLength)
-                    {
-                        loadsOnBridge.Add(axleLoads[j]);
-                        axlesOnBridge.Add(axlePositions[j]);
-                    }
-                }
-
-                double[] moments = new double[loadsOnBridge.Count()];
-                double[] shears = new double[loadsOnBridge.Count()];
-                for (int j = 0; j < loadsOnBridge.Count(); j++)
-                {
-                    moments[j] = Moment(loadsOnBridge[j], xLocation, axlesOnBridge[j], spanLength);
-                    shears[j] = Shear(loadsOnBridge[j], xLocation, axlesOnBridge[j], spanLength);
-                }
-
-                double momentTotal = moments.Sum();
-                double shearTotal = Math.Abs(shears.Sum());
-
-                if (momentTotal > mMaxVal)
-                {
-                    mMaxVal = momentTotal;
-                    mMaxLoc = axlePositions[0];
-                }
-                if (shearTotal > vMaxVal)
-                {
-                    vMaxVal = shearTotal;
-                    vMaxLoc = axlePositions[0];
-                }
-                for (int j = 0; j < axlePositions.Count(); j++)
-                {
-                    axlePositions[j] = axlePositions[j] + incr;
-                }
-            }
-
-            double mMax = mMaxVal * (1 + impactFactor) * distFactor;
-            double vMax = vMaxVal * (1 + impactFactor) * distFactor;
-            Dictionary<string, double> result = new Dictionary<string, double>();
-            result.Add("m", mMax);
-            result.Add("v", vMax);
-            result.Add("mloc", mMaxLoc);
-            result.Add("vloc", vMaxLoc);
-            return result;
-        }
-
-        public void Calculate()
-        {
-            double spanLength = 0;
-            double xLocation = 0;
-            bool? feet = false;
-            bool? pct = false;
-
-            try
-            {
-                spanLength = double.Parse(SpanLength.Text);
                 xLocation = double.Parse(xLoc.Text);
-                feet = RadioFeet.IsChecked;
                 pct = RadioPct.IsChecked;
-            }
-            catch (Exception)
+                analysis.Span = double.Parse(SpanLength.Text);
+                analysis.IncrementInches = double.Parse(Increment.Text);
+                analysis.ImpactFactor = double.Parse(ImpactFactor.Text);
+                analysis.DistFactor = double.Parse(DistributionFactor.Text);
+                analysis.GetTrainType(LoadType.SelectedIndex);
+            } catch (Exception)
             {
                 MessageBox.Show("Invalid input!");
+                return;
             }
+
+            // if the percent radio button is checked, set the xLocation value to its value times the span length
             if ((bool)pct)
             {
-                xLocation = spanLength * xLocation;
+                xLocation = analysis.Span * xLocation;
             }
 
-            Dictionary<string,double> vals = CalculateLocation(xLocation);
+            analysis.GetTrain();
+            Dictionary<string, double> vals = analysis.CalculateSingleLocation(xLocation);
             double mMax = vals["m"];
             double vMax = vals["v"];
             double mMaxLoc = vals["mloc"];
@@ -219,119 +117,82 @@ namespace MVCalc
             ShearResult.Text = $"The maximum shear {vMax:0.00} k occurs while the front of the train is at {vMaxLoc:0.00} ft.";
         }
 
-        public void CalculatePlots()
-        {
-            double spanLength = 0;
-            Tuple<double[], double[]> vals = new Tuple<double[], double[]>(null,null);
-            vals = CalculateEnv();
-            try
-            {
-                spanLength = double.Parse(SpanLength.Text);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Invalid input!");
-            }
-
-            double[] maxMs = vals.Item1;
-            double[] maxVs = vals.Item2;
-            
-            MPlot = MakePlot("Moments", maxMs, spanLength);
-            VPlot = MakePlot("Shears", maxVs, spanLength);
-        }
-
-        public Tuple<double[], double[]> CalculateEnv()
-        {
-            double spanLength = 0;
-            int plotPoints = 0;
-
-            try
-            {
-                spanLength = double.Parse(SpanLength.Text);
-                plotPoints = int.Parse(PlotPoints.Text);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Invalid input!");
-            }
-
-
-            double[] locations = new double[plotPoints + 1];
-            double[] mMaxs = new double[plotPoints + 1];
-            double[] vMaxs = new double[plotPoints + 1];
-
-            for (int i = 0; i < plotPoints+1; ++i)
-            {
-                locations[i] = spanLength / (plotPoints) * i;
-                Dictionary<string, double> vals = CalculateLocation(locations[i]);
-                mMaxs[i] = vals["m"];
-                vMaxs[i] = vals["v"];
-            }
-
-            return new Tuple<double[], double[]>(mMaxs, vMaxs);
-        }
-
-        public static PlotModel MPlot { get; set; }
-
-        public static PlotModel VPlot { get; set; }
-
-        public void clear()
+        public void ClearClick(object sender, RoutedEventArgs e)
         {
             MomentResult.Text = "";
             ShearResult.Text = "";
         }
 
-        public void calculateClick(object sender, RoutedEventArgs e)
+        public void PlotClick(object sender, RoutedEventArgs e)
         {
-            Calculate();
-        }
+            // declare/instantiate variables
+            E80Analysis analysis = new E80Analysis();
+            int plotPoints;
 
-        public void clearClick(object sender, RoutedEventArgs e)
-        {
-            clear();
-        }
-
-        public void plotClick(object sender, RoutedEventArgs e)
-        {
+            // get user inputs
             try
             {
-                CalculatePlots();
-                PlotWindow plots = new PlotWindow();
-                plots.Show();
+                plotPoints = int.Parse(PlotPoints.Text);
+                analysis.Span = double.Parse(SpanLength.Text);
+                analysis.IncrementInches = double.Parse(Increment.Text);
+                analysis.ImpactFactor = double.Parse(ImpactFactor.Text);
+                analysis.DistFactor = double.Parse(DistributionFactor.Text);
+                analysis.GetTrainType(LoadType.SelectedIndex);
             }
             catch (Exception)
             {
                 MessageBox.Show("Invalid input!");
                 return;
             }
+
+            analysis.GetTrain();
+            Tuple<double[], double[]> vals = analysis.CalculateEnvelope(plotPoints);
+
+            double[] maxMs = vals.Item1;
+            double[] maxVs = vals.Item2;
+
+            MPlot = MakePlot("Moments", maxMs, analysis.Span);
+            VPlot = MakePlot("Shears", maxVs, analysis.Span);
+
+            PlotWindow plots = new PlotWindow();
+            plots.Show();
         }
 
-        public void valClick(object sender, RoutedEventArgs e)
+        public void ValClick(object sender, RoutedEventArgs e)
         {
-            double spanLength = 0;
-            Tuple<double[], double[]> vals = new Tuple<double[], double[]>(null, null);
+            // declare/instantiate variables
+            E80Analysis analysis = new E80Analysis();
+            int plotPoints;
 
+            // get user inputs
             try
             {
-                vals = CalculateEnv();
-                spanLength = double.Parse(SpanLength.Text);
+                plotPoints = int.Parse(PlotPoints.Text);
+                analysis.Span = double.Parse(SpanLength.Text);
+                analysis.IncrementInches = double.Parse(Increment.Text);
+                analysis.ImpactFactor = double.Parse(ImpactFactor.Text);
+                analysis.DistFactor = double.Parse(DistributionFactor.Text);
+                analysis.GetTrainType(LoadType.SelectedIndex);
             }
             catch (Exception)
             {
                 MessageBox.Show("Invalid input!");
+                return;
             }
+
+            analysis.GetTrain();
+            Tuple<double[], double[]> vals = analysis.CalculateEnvelope(plotPoints);
 
             double[] maxMs = vals.Item1;
             double[] maxVs = vals.Item2;
 
             TextResultsWindow results = new TextResultsWindow();
 
-            int numPoints = maxMs.Length;
-            double[] locations = new double[numPoints];
+            double[] locations = new double[plotPoints + 1];
 
-            for (int i = 0; i < numPoints; ++i)
+            for (int i = 0; i < plotPoints + 1; ++i)
             {
-                locations[i] = (spanLength / (numPoints - 1) * i);
+                locations[i] = analysis.Span / (plotPoints) * i;
             }
 
             results.Locations.Text = MakeList(locations);
